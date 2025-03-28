f73327125591414e4bc9c27d9eb35c143c732b3d
JoeAshton <thebingitkid@gmail.com>
2025-03-28 14:23:04 +0000

    target_move

--- src/test_move.m
+++ src/test_move.m
@@ -1,24 +1,11 @@
-
-body7 = rigidBody('body7');
-jnt7 = rigidBodyJoint('jnt7', 'fixed');
-tform7 = trvec2tform([0, 0, 0.08])*eul2tform([0, 0, 0]);
-setFixedTransform(jnt7,tform7);
-body7.Joint = jnt7;
-addBody(robot,body7,'body6');
-
 show(robot);
 axis tight;
 
 pos = [0.15 0.15 0.005];
-poseTF = trvec2tform(pos);
-
-hold on;
-plotTransforms(pos,eul2quat([0 0 0]),FrameSize=0.1);
-
-ik = inverseKinematics("RigidBodyTree",robot);
+angles = [0 0 0];
 weights = [0 1 1 1 1 1];
-initialguess = homeConfiguration(robot);
-[configSoln,solnInfo] = ik("body7",poseTF,weights,initialguess);
+
+configSoln = target_move(robot, pos, angles, weights);
 
 figure;
 show(robot,configSoln,PreservePlot=false);
