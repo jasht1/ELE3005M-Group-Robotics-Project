2b2921f68f16158cb50a33229387e673fe0100b0
Joe LenovoLegionPopOs <thebingitkid@gmail.com>
2025-05-04 16:30:14 +0100

    update target_move formatting

--- src/target_move.m
+++ src/target_move.m
@@ -1,6 +1,6 @@
-function configSoln = target_move(robot, pos, angles, weights)
-poseTF = trvec2tform(pos) * eul2tform(angles);
-ik = inverseKinematics("RigidBodyTree",robot);
-initialguess = homeConfiguration(robot);
-[configSoln, ~] = ik("body7",poseTF,weights,initialguess);
+function pose = target_move(robot, position, angle, weights)
+  poseTF = trvec2tform(position) * eul2tform(angle);
+  ik = inverseKinematics("RigidBodyTree",robot);
+  initialguess = homeConfiguration(robot);
+  [pose, ~] = ik("body7",poseTF,weights,initialguess);
 end
