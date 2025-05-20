6176e401d37c92a7407f91e17f4af29adae28da9
Joe LenovoLegionPopOs <thebingitkid@gmail.com>
2025-05-20 19:21:03 +0100

    simplify animation script

--- src/stack_blocks_smooth_animation.m
+++ src/stack_blocks_smooth_animation.m
@@ -2,19 +2,17 @@ robot = get_nyro_body();
 positions = get_positions();
 [qMat, ts] = plan_trajectory(robot, positions);
 
-% for i = 1:size(qMat, 1)
-%     config = homeConfiguration(robot);
-%     for j = 1:numel(config)
-%         config(j) = qMat(i, j);
-%     end
-%     show(robot, config, 'PreservePlot', false, 'Frames', 'on');
-%     xlim([-0.1, 0.4]);
-%     ylim([-0.3, 0.3]);
-%     zlim([0, 0.5]);
-%     drawnow;
-% end
-
 function animate_trajectory(robot, qMat, save)
+    if save
+        v = VideoWriter(videoFilename, 'Motion JPEG AVI'); %use MPEG-4 if u use win
+        v.Quality = 100;
+        v.FrameRate = fps;
+        open(v);
+        showfig = 'off'
+    else
+        showfig = 'on'
+    end
+
     % Set plot limits
     xLimits = [-0.1, 0.4];
     yLimits = [-0.3, 0.3];
@@ -24,14 +22,7 @@ function animate_trajectory(robot, qMat, save)
     % Video settings
     videoFilename = 'Exports/stack_blocks_smooth_animation';
     fps = 30;
-    fig = figure('Visible', ternary(save, 'off', 'on'), 'Position', [100, 100, 1920, 1080]);
-
-    if save
-        v = VideoWriter(videoFilename, 'Motion JPEG AVI'); %use MPEG-4 if u use win
-        v.Quality = 100;
-        v.FrameRate = fps;
-        open(v);
-    end
+    fig = figure('Visible', showfig, 'Position', [100, 100, 1920, 1080]);
 
     for i = 1:size(qMat, 1)
         config = homeConfiguration(robot);
@@ -55,14 +46,6 @@ function animate_trajectory(robot, qMat, save)
     if save
         close(v);
         fprintf('Animation saved to %s\n', videoFilename);
-    end
-end
-
-function result = ternary(condition, valTrue, valFalse)
-    if condition
-        result = valTrue;
-    else
-        result = valFalse;
     end
 end
 
